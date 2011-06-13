ffe41225856e64680d4401ec50793572d1235f79
yfsuoyou <yfsuoyou@3338bcd4-1a09-11de-8120-8d224146dbbc>
2011-06-13 16:14:37 +0000

    初步完成更新模块代码。

--- Application/Updater/Updater/Update.cpp
+++ Application/Updater/Updater/Update.cpp
@@ -3,27 +3,38 @@
 #include "curl\curl.h"
 #include "curl\types.h"
 #include "curl\easy.h"
+#include "tinyxml.h"
+#include <iostream>
 
+using namespace std;
+
+#define MAXFILELENGTH 1024
 struct FtpFile {
 	const char *filename;
 	FILE *stream;
 };
 
+
+
+char xmlString[MAXFILELENGTH];
+char version[20];
+
+
 const char * SERVER1 = "http://mygooglecode.googlecode.com/files/";
 
-static int my_fwrite(void *buffer, size_t size, size_t nmemb, void *stream)
+static int writer(char *data, size_t size, size_t nmemb,char *writerData)
 {
-	struct FtpFile *out=(struct FtpFile *)stream;
-	if(out && !out->stream) {
-		out->stream=fopen(out->filename, "wb");
-		if(!out->stream)
-			return -1;
-	}
-	return fwrite(buffer, size, nmemb, out->stream);
+	if (writerData == NULL)
+		return 0;
+	if(size*nmemb > MAXFILELENGTH)
+		return 0;
+	memcpy(writerData,data,size*nmemb);
+	return size * nmemb;
 }
 
 bool Init(const char * version,const char * configName)
 {
+	strcpy(::version,version);
 	LONG lResult = 0L;
 	char pRemotePath[100];
 	strcpy(pRemotePath,SERVER1);
@@ -40,8 +51,8 @@ bool Init(const char * version,const char * configName)
 	curl = curl_easy_init();
 	if(curl) {
 		curl_easy_setopt(curl, CURLOPT_URL,pRemotePath);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ftpfile);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &xmlString);
 #ifdef _DEBUG
 		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 #endif
@@ -58,7 +69,40 @@ bool Init(const char * version,const char * configName)
 	return 0==lResult;
 }
 
+int VerCmp(const char * ver1,const char * ver2)
+{
+	return strcmp(ver1,ver2);
+}
+
 bool GetUpdateInfo(PUPDATEINFO pUpdateInfo)
 {
+	pUpdateInfo->updateFlag = U_NO;
+	strcpy(pUpdateInfo->newVersion,"0.0");
+	strcpy(pUpdateInfo->updateUrl,"http://www.yangfei.org");
+
+	TiXmlDocument doc;
+	int iRecommend = 0;
+	doc.Parse(xmlString);
+	TiXmlNode *pNode = doc.RootElement()->FirstChild();
+	while(pNode != NULL)
+	{
+		if(strcmp(pNode->Value(),"LastVersion") == 0)
+		{
+			strcpy(pUpdateInfo->newVersion,pNode->FirstChild()->Value());
+		}
+		if(strcmp(pNode->Value(),"Recommend") == 0)
+		{
+			iRecommend = atoi(pNode->FirstChild()->Value());
+		}
+		if(strcmp(pNode->Value(),"UpdateUrl") == 0)
+		{
+			strcpy(pUpdateInfo->updateUrl,pNode->FirstChild()->Value());
+		}
+		pNode = pNode->NextSibling();
+	}
+	if(VerCmp(pUpdateInfo->newVersion,version) > 0)
+	{
+		pUpdateInfo->updateFlag = UPDATEFLAG(U_YES + iRecommend);
+	}
 	return false;
 }
