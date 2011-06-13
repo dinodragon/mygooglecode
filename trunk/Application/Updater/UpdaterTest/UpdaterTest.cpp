f49005f54e7a12a4a11aa1fc3a068b40a4b2c334
yfsuoyou <yfsuoyou@3338bcd4-1a09-11de-8120-8d224146dbbc>
2011-06-13 13:44:25 +0000

    添加更新管理器程序

---
+++ trunk/Application/Updater/UpdaterTest/UpdaterTest.cpp
@@ -0,0 +1,13 @@
+// UpdaterTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "..\Updater\updater.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	bool a = Init("1.6","Base64.xml");
+	return 0;
+}
+
