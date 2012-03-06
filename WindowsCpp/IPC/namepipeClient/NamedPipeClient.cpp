de97bdac42fb96ba6464b555f4575f7dd2fbbefc
yfsuoyou <yfsuoyou@3338bcd4-1a09-11de-8120-8d224146dbbc>
2012-03-06 09:00:29 +0000

    namedpipe demo cpp

---
+++ WindowsCpp/IPC/namepipeClient/NamedPipeClient.cpp
@@ -0,0 +1,76 @@
+﻿#include "NamedPipeClient.h"
+#include <iostream>
+
+CNamedPipeClient::CNamedPipeClient():hPipe_(NULL)
+{
+}
+
+CNamedPipeClient::~CNamedPipeClient(void)
+{
+	if (hPipe_)
+	{
+		CloseHandle(hPipe_);
+	}
+}
+
+bool CNamedPipeClient::Create(const char * szPipName)
+{
+	pipeName_ = szPipName;
+	return true;
+}
+
+std::string CNamedPipeClient::Send(std::string cmd)
+{
+	std::string result = "";
+	if(!WaitNamedPipe(pipeName_.c_str(), NMPWAIT_WAIT_FOREVER))
+	{
+		std::cout<<"WaitNamedPipe failed!"<<std::endl;
+		return result;
+	}
+	hPipe_ = CreateFile(
+		pipeName_.c_str(),
+		GENERIC_READ | GENERIC_WRITE, 
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+		);
+	if(INVALID_HANDLE_VALUE == hPipe_)
+	{
+		std::cout<<"CreateFile failed!"<<std::endl;
+		return result;
+	}
+	DWORD                dwWrite;
+	//向命名管道中写入数据
+	if(!WriteFile(hPipe_, cmd.c_str(),cmd.length(), &dwWrite, NULL))
+	{
+		std::cout<<"WriteFile failed!"<<std::endl;
+		return result;
+	}
+
+	DWORD dwRead;
+	char * pReadBuf = new char[BUFSIZE + 1];
+	memset(pReadBuf, 0,BUFSIZE + 1);
+	//从命名管道中读取数据
+	if(!ReadFile(hPipe_, pReadBuf,BUFSIZE, &dwRead, NULL))
+	{
+		delete []pReadBuf;
+		std::cout<<"ReadFile failed!"<<std::endl;
+		return result;
+	}
+	result = std::string(pReadBuf);
+	delete []pReadBuf;
+	return result;
+}
+
+
+bool CNamedPipeClient::Stop()
+{
+	return false;
+}
+
+bool CNamedPipeClient::Close()
+{
+	return false;
+}
--- WindowsCpp/IPC/namepipeClient/namepipeClient.cpp
+++ WindowsCpp/IPC/namepipeClient/namepipeClient.cpp
@@ -3,80 +3,16 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "namedPipeClient.h"
 #include <Windows.h>
-
-using namespace std;
-
-HANDLE            hNamedPipe;
-const char * pStr        = "Zachary";
-const char * pPipeName    = "\\\\.\\pipe\\ZacharyPipe";
-
-//打开命名管道
-void OpenNamedPipeInClient();
-//客户端从命名管道中读取数据
-void NamedPipeReadInClient();
-//客户端往命名管道中写入数据
-void NamedPipeWriteInClient();
+#include <iostream>
 
 int main(int argc, char * argv)
 {
-	OpenNamedPipeInClient();
-	//接收服务端发来的数据
-	NamedPipeReadInClient();
-	//往命名管道中写入数据
-	NamedPipeWriteInClient();
-	system("pause");
-}
-
-void OpenNamedPipeInClient()
-{
-	//等待连接命名管道
-	if(!WaitNamedPipe(pPipeName, NMPWAIT_WAIT_FOREVER))
-	{
-		cout<<"命名管道实例不存在 ..."<<endl<<endl;
-		return;
-	}
-	//打开命名管道
-	hNamedPipe = CreateFile(pPipeName, GENERIC_READ | GENERIC_WRITE, 
-		0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if(INVALID_HANDLE_VALUE == hNamedPipe)
-	{
-		cout<<"打开命名管道失败 ..."<<endl<<endl;
-		return;
-	}
-}
- 
- 
-void NamedPipeReadInClient()
-{
-	char *                pReadBuf;
-	DWORD                dwRead;
-	 
-	pReadBuf = new char[strlen(pStr) + 1];
-	memset(pReadBuf, 0, strlen(pStr) + 1);
-	 
-	//从命名管道中读取数据
-	if(!ReadFile(hNamedPipe, pReadBuf, strlen(pStr), &dwRead, NULL))
-	{
-		delete []pReadBuf;
-		 
-		cout<<"读取数据失败 ..."<<endl<<endl;
-		return;
-	}
-	cout<<"读取数据成功：    "<<pReadBuf<<endl<<endl;
-}
- 
- 
-void NamedPipeWriteInClient()
-{
-	DWORD                dwWrite;
-	 
-	//向命名管道中写入数据
-	if(!WriteFile(hNamedPipe, pStr, strlen(pStr), &dwWrite, NULL))
-	{
-		cout<<"写入数据失败 ..."<<endl<<endl;
-		return;
-	}
-	cout<<"写入数据成功：    "<<pStr<<endl<<endl;
+	const char * pPipeName    = "\\\\.\\pipe\\ZacharyPipe";
+	CNamedPipeClient client;
+	client.Create(pPipeName);
+	std::string r = client.Send("aaaaa!");
+	std::cout<<r.c_str()<<std::endl;
 }
 
