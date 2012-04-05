b8a2c7050621d025ca4e422229ed33fc74320055
yfsuoyou <yfsuoyou@3338bcd4-1a09-11de-8120-8d224146dbbc>
2012-04-05 08:37:32 +0000

    vontroldemo

---
+++ Objective-C/HelloObjC/ControlDemo/ControlDemo/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  ControlDemo
+//
+//  Created by 飞 杨 on 12-4-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
